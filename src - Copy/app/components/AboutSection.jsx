a6d0106c5712ed7aa719e6c1e38510481710668f
Sajjad Ahmad <109279767+sajjadahmadquershi@users.noreply.github.com>
2024-04-19 14:56:06 +0500

    Update AboutSection.jsx

--- src - Copy/app/components/AboutSection.jsx
+++ src - Copy/app/components/AboutSection.jsx
@@ -56,18 +56,17 @@ const AboutSection = () => {
     {/* <Image src="/images/about-image.png" width={500} height={500} /> */}
     {/* <video src="" width={500} height={500} /> */}
     <div className="relative" style={{ maxWidth: '100%' }}>
-      <iframe
-        className="w-full"
-        width="500"
-        height="315"
-        src="https://www.youtube.com/embed/X7XJX_hky80?si=e28byYpwpMlsjA4G"
-        title="YouTube video player"
-        frameBorder="1"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
-    </div>
+          <iframe
+            className="w-full" // Maintain full width for responsive behavior
+            src="https://www.youtube.com/embed/X7XJX_hky80?si=e28byYpwpMlsjA4G"
+            title="YouTube video player"
+            frameBorder="1"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            style={{ width: '-webkit-fill-available' }} // Add style directly
+          />
+        </div>
     <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
       <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
       <p className="text-base lg:text-lg">
