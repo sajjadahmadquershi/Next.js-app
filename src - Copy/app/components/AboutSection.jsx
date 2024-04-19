087efff8ef8608e94eadc15a5ffdf36fb75d4f1e
Sajjad Ahmad <109279767+sajjadahmadquershi@users.noreply.github.com>
2024-04-19 13:18:19 +0500

    Update AboutSection.jsx

--- src - Copy/app/components/AboutSection.jsx
+++ src - Copy/app/components/AboutSection.jsx
@@ -51,49 +51,61 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        {/* <Image src="/images/about-image.png" width={500} height={500} /> */}
-        {/* <video src="" width={500} height={500} /> */}
-        <iframe width="500" height="315" src="https://www.youtube.com/embed/4Zibxexituk?si=cKL_9CtV9ut74KHF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            I specialize in 3D design, blending creativity with technical precision to craft captivating visual experiences.
-            With expertise in SolidWorks, Revit, and Lumion, I bring ideas to life, from intricate mechanical
-            components to architectural wonders. My passion for innovation extends to 3D printing, where I
-            leverage cutting-edge technology to turn concepts into tangible realities. collaborate to turn your
-            visions into stunning 3D-printed masterpieces.
-          </p>
-          <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              Certifications{" "}
-            </TabButton>
-          </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
-          </div>
-        </div>
+   <section className="text-white" id="about">
+  <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+    {/* <Image src="/images/about-image.png" width={500} height={500} /> */}
+    {/* <video src="" width={500} height={500} /> */}
+    <div className="relative" style={{ maxWidth: '100%' }}>
+      <iframe
+        className="w-full"
+        width="500"
+        height="315"
+        src="https://www.youtube.com/embed/X7XJX_hky80?si=e28byYpwpMlsjA4G"
+        title="YouTube video player"
+        frameBorder="1"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+    <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+      <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+      <p className="text-base lg:text-lg">
+        I specialize in 3D design, blending creativity with technical precision to craft captivating visual experiences.
+        With expertise in SolidWorks, Revit, and Lumion, I bring ideas to life, from intricate mechanical
+        components to architectural wonders. My passion for innovation extends to 3D printing, where I
+        leverage cutting-edge technology to turn concepts into tangible realities. Let s collaborate to turn your
+        visions into stunning 3D-printed masterpieces.
+      </p>
+      <div className="flex flex-row justify-start mt-8">
+        <TabButton
+          selectTab={() => handleTabChange("skills")}
+          active={tab === "skills"}
+        >
+          {" "}
+          Skills{" "}
+        </TabButton>
+        <TabButton
+          selectTab={() => handleTabChange("education")}
+          active={tab === "education"}
+        >
+          {" "}
+          Education{" "}
+        </TabButton>
+        <TabButton
+          selectTab={() => handleTabChange("certifications")}
+          active={tab === "certifications"}
+        >
+          {" "}
+          Certifications{" "}
+        </TabButton>
       </div>
-    </section>
+      <div className="mt-8">
+        {TAB_DATA.find((t) => t.id === tab).content}
+      </div>
+    </div>
+  </div>
+</section>
   );
 };
 
